e40067e9af97f877f735a49a31cfe43f4db18c6a
Kooten <bostrom91@gmail.com>
2023-11-04 23:37:43 +0100

    Multi OS support

--- convert-and-quant.py
+++ convert-and-quant.py
@@ -10,7 +10,8 @@ parser = argparse.ArgumentParser(description='Convert and Quantize fp16 models t
 parser.add_argument('model_path', type=str, help='Path to FP16 model directory')
 args = parser.parse_args()
 
-with open("util\settings.json", 'r') as f:
+settings_path = os.path.join('util', 'settings.json')
+with open(settings_path, 'r') as f:
     config = json.load(f)
 
 

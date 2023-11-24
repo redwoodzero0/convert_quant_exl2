ba4da35f9b480efb0c78dba4fdcada3f1b0bb909
redwoodzero0 <150784533+redwoodzero0@users.noreply.github.com>
2023-11-25 03:55:33 +0900

    Update convert-and-quant.py

--- convert-and-quant.py
+++ convert-and-quant.py
@@ -14,36 +14,18 @@ settings_path = os.path.join('/workspace/exllamav2/util', 'settings.json')
 with open(settings_path, 'r') as f:
     config = json.load(f)
 
-
-# https://github.com/turboderp/exllamav2/blob/master/doc/convert.md
 fp16_model_dir = args.model_path
-cal_dataset = config["cal_dataset"] # https://huggingface.co/datasets/wikitext
-bits_per_weight = config["bits_per_weight"] # Default = 4.65
-head_bits = config["head_bits"] # Default = 6
-gpu_rows = config["gpu_rows"] # Default = 0
-token_length = config["token_length"] # Default = 2048
-measurement_length = config["measurement_length"] # Default = 2048
-rope_alpha = config["rope_alpha"] # Default = 1
+cal_dataset = config["cal_dataset"]
+bits_per_weight = config["bits_per_weight"]
+head_bits = config["head_bits"]
+gpu_rows = config["gpu_rows"]
+token_length = config["token_length"]
+measurement_length = config["measurement_length"]
+rope_alpha = config["rope_alpha"]
 quant_dir = os.path.join(fp16_model_dir, f"{os.path.basename(fp16_model_dir)}-{bits_per_weight}bpw-h{head_bits}-exl2")
 util_dir = os.path.dirname(os.path.abspath(__file__))
 exllama_dir = os.path.dirname(util_dir)
 measurement_file = os.path.join(exllama_dir, f"measurement-{os.path.basename(fp16_model_dir)}.json")
-conversion_script = os.path.join(exllama_dir, 'util', 'convert_safetensors.py')
-
-
-def convert_bin_to_safetensors(fp16_model_dir):
-    for f in os.listdir(fp16_model_dir):
-        if f.endswith('.bin'):
-            basename = os.path.splitext(f)[0]
-            safetensor_file = f"{basename}.safetensors"
-            if os.path.exists(os.path.join(fp16_model_dir, safetensor_file)):
-                print(f"Skipping conversion of {f} as the safetensor file already exists")
-                continue
-            try:
-                print(f"converting {basename}.bin to {basename}.safetensor...")
-                subprocess.run(['python', conversion_script, os.path.join(fp16_model_dir, f)])
-            except Exception as e:
-                print(f"Error in converting {f}: {e}")
 
 def clean_up_leftovers(directory):
     print("Remove out_tensor/")
@@ -68,8 +50,6 @@ def copy_model_config():
     model_files = [f for f in os.listdir(fp16_model_dir) if f.endswith('.json') or f.startswith('tokenizer.')]
     for model_file in model_files:
         shutil.copy(os.path.join(fp16_model_dir, model_file), os.path.join(quant_dir, model_file))
-
-convert_bin_to_safetensors(fp16_model_dir)
 
 if os.path.exists(quant_dir):
     print(f"Target output directory already exists: {quant_dir}")
